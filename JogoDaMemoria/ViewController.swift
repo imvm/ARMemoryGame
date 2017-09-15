c9451eb9ac10de5544e99b7b33652206d281774f
imvm <imvm@cin.ufpe.br>
2017-09-15 09:30:40 -0300

    detecting barcodes

--- JogoDaMemoria/ViewController.swift
+++ JogoDaMemoria/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController, ARSKViewDelegate {
         sceneView.showsNodeCount = true
         
         // Load the SKScene from 'Scene.sks'
-        if let scene = SKScene(fileNamed: "Scene") {
+        if let scene = BarcodeDetectionScene(fileNamed: "BarcodeDetectionScene") {
             sceneView.presentScene(scene)
         }
     }
